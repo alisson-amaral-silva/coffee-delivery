0954ca27899326a60773d352ca66e65580a29859
Alisson Amaral <aalissonamaral98@gmail.com>
2023-01-05 12:02:40 -0300

    ajustando img do expresso padrao e seu wrapper

--- src/pages/Home/components/CoffeeCard/styles.ts
+++ src/pages/Home/components/CoffeeCard/styles.ts
@@ -1,15 +1,14 @@
 import styled from 'styled-components'
 
 export const CoffeeWrapper = styled.div`
-  max-width: 20rem;
-  width: 15rem;
-  height: 19.3rem;
+  max-width: 15rem;
+  height: 20.3rem;
   background: ${(props) => props.theme['base-card']};
   border-radius: 6px 36px;
   display: flex;
   align-items: center;
   flex-direction: column;
-  gap: 1.5rem;
+  gap: 1.05rem;
   img {
     padding: 0;
   }
